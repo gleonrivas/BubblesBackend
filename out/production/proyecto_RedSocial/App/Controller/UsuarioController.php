1976cff96a75246c7eee42bffb7f24a981ce747a
celim <celimva@gmail.com>
2023-01-18 18:45:06 +0100

    mensaje, token

---
+++ out/production/proyecto_RedSocial/App/Controller/UsuarioController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\UsuarioEntityRepository;
+use App\Utilidades\Utilidades;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UsuarioController extends AbstractController
+{
+
+    #[Route('/usuario', name: 'app_usuario')]
+    public function index(): JsonResponse
+    {
+       return $this->json([
+           'message' => 'Controller',
+           'path' => 'src/Controller/UsuarioController.php'
+       ]);
+
+    }
+    #[Route('/usuario/listar', name: 'app_usuario_listar', methods: ['GET'])]
+    public function listar(UsuarioEntityRepository $repository, Utilidades $utils):JsonResponse
+    {
+        $lista_usuarios = $repository->findAll();
+        $lista_Json = json_encode($lista_usuarios);
+
+        //$lista_Json = $utils->toJson($lista_usuarios);
+
+        return new JsonResponse($lista_Json, 200,[], true);
+    }
+}
---
+++ out/production/proyecto_RedSocial/App/Repository/UsuarioEntityRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\UsuarioEntity;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<UsuarioEntity>
+ *
+ * @method UsuarioEntity|null find($id, $lockMode = null, $lockVersion = null)
+ * @method UsuarioEntity|null findOneBy(array $criteria, array $orderBy = null)
+ * @method UsuarioEntity[]    findAll()
+ * @method UsuarioEntity[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UsuarioEntityRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, UsuarioEntity::class);
+    }
+
+    public function save(UsuarioEntity $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(UsuarioEntity $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return UsuarioEntity[] Returns an array of UsuarioEntity objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('m')
+//            ->andWhere('m.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('m.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?UsuarioEntity
+//    {
+//        return $this->createQueryBuilder('m')
+//            ->andWhere('m.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
