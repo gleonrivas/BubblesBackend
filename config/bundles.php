bc0602cd5e9b352e2b0b4cc98b76c31a854cf8ce
gonzalo <gleonrivas@safareyes.es>
2023-02-07 13:55:31 +0100

    paquetes?

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
